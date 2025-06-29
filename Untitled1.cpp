c32030897a2f65d5c4548da1b8d178e6188e91a9
DENNESM21 <dennes.molina@epn.edu.ec>
2025-06-29 18:04:56 -0500

    Test

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -5,15 +5,20 @@
 using namespace std;
 
 void Mostrar_menu (){
+int opcion;
 cout<<"====Bienvenido a tiendas LLS===="<<endl;
 cout<<"1.-Registrar Nuevo Cliente"<<endl;
 cout<<"2.-Mostrar estado de Cuenta"<<endl;
 cout<<"3.-Ingresar datos"<<endl;
-	
+cout<<"4.-Salir"<<endl;
+cout<<"Ingrese una opcion: ";
+cin>>opcion;
 }
 
 
 int main(){
-	
-	
+	Mostrar_menu();
+	return 0;
 }
+	
+
