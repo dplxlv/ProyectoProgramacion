96e6b4ca261ee6541e2bd5c215aeb6ea9640e679
dplxlv <dplxlv12@gmail.com>
2025-06-28 23:58:17 -0500

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+void Mostrar_menu (){
+cout<<"====Bienvenido a tiendas LLS===="<<endl;
+cout<<"1.-Registrar Nuevo Cliente"<<endl;
+cout<<"2.-Mostrar estado de Cuenta"<<endl;
+cout<<"3.-Ingresar datos"<<endl;
 
 
 int main(){
